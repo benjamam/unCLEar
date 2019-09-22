d7c60800d15538b79f2c9eda9e0d6e3efc210254
benjamam <benjamam@miamioh.edu>
2019-09-22 10:55:46 -0400

    latest

--- src/app/modules/home/components/filter/filter.component.ts
+++ src/app/modules/home/components/filter/filter.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  filterType: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showSecondFilter(filterType: string) {
+    this.filterType = filterType;
   }
 
 }
