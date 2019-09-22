8d46f28d1b0e9f9a353dbb85222110cc95d010d3
benjamam <benjamam@miamioh.edu>
2019-09-21 23:43:24 -0400

    set up routing, components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { ProposalComponent } from './modules/proposal.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'discover',
+    component: HomeComponent
+  },
+  {
+    path: 'propose',
+    component: ProposalComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'discover'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './modules/home/components/card/card.component';
+import { HomeComponent } from './modules/home/home.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { FilterComponent } from './modules/home/components/filter/filter.component';
+import { ProposalComponent } from './modules/proposal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent,
+    HeaderComponent,
+    FilterComponent,
+    ProposalComponent
   ],
   imports: [
     BrowserModule,
