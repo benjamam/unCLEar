64ee4a39c94fa8a67a7e402ad249293eb316819d
benjamam <benjamam@miamioh.edu>
2019-09-22 15:35:10 -0400

    routing from homepage

--- src/app/modules/home/components/splash-page/splash-page.component.ts
+++ src/app/modules/home/components/splash-page/splash-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash-page',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash-page.component.scss']
 })
 export class SplashPageComponent implements OnInit {
-
-  constructor() { }
+  router: Router;
+  constructor(router: Router) {
+    this.router = router;
+  }
 
   ngOnInit() {
   }
 
+  routeTo(r: string) {
+    if (r === 'projects') {
+      this.router.navigate(['discover/projects']);
+    }
+    else if (r === 'artists') {
+      this.router.navigate(['discover-artists']);
+    }
+    else if (r === 'propose') {
+      this.router.navigate(['propose']);
+
+    }
+  }
 }
